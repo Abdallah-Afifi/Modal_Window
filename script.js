ecc7bf95b3b33cac32667de22ce3a060bb798a59
Abdallah Afifi <abdallahfathi@aucegypt.edu>
2025-03-26 18:34:53 -0400

    added main sections of window then sliding main

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 var modal = document.getElementById('modal');
-
 var buttonO = document.getElementById('button');
-
 var buttonC = document.getElementsByClassName('close')[0];
+var cancelBtn = document.getElementById('cancel-btn');
+var confirmBtn = document.getElementById('confirm-btn');
 
 buttonO.onclick = function() {
     modal.style.display = 'block';
     modal.setAttribute('aria-hidden', 'false');
 }
 
-buttonC.onclick = function() {
+function closeModal() {
     modal.style.display = 'none';
     modal.setAttribute('aria-hidden', 'true');
 }
 
+buttonC.onclick = closeModal;
+cancelBtn.onclick = closeModal;
+
+confirmBtn.onclick = function() {
+    alert('Action confirmed!');
+    closeModal();
+}
+
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = 'none';
-        modal.setAttribute('aria-hidden', 'true');
+        closeModal();
     }
 }
